38756b156247c451b962a58be0802082525fe13f
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-07-27 20:22:47 -0300

    feat: create register ui  validation

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { useRegisterController } from "./useRegisterController";
 
 export function Register(){
+  const {errors, handleSubmit, register} = useRegisterController();
   return (
       <>
         <h1 className="text-gray-900 text-2xl font-bold mb-4 tracking-[-1px]">Crie sua conta</h1>
@@ -10,10 +12,10 @@ export function Register(){
           <span className="text-gray-700 tracking-[-0.5px]">Já possui uma conta?</span>
           <Link to={'/login'} className="font-medium text-teal-900 tracking-[-0.5px]">Fazer Login</Link>
         </p>
-        <form className="w-full mt-[60px] flex flex-col gap-4">
-          <Input name="name" type="name" placeholder="Nome"/>
-          <Input name="email" type="email" placeholder="E-mail"/>
-          <Input name="password" type="password" placeholder="Senha"/>
+        <form onSubmit={ handleSubmit } className="w-full mt-[60px] flex flex-col gap-4">
+          <Input error={errors.name?.message} {...register("name")} type="name" placeholder="Nome"/>
+          <Input error={errors.email?.message} {...register("email")} type="email" placeholder="E-mail"/>
+          <Input error={errors.password?.message} {...register("password")} type="password" placeholder="Senha"/>
           <Button type="submit" className="mt-2 h-[54px]">Criar conta</Button>
         </form>
       </>
---
+++ src/view/pages/Register/useRegisterController.tsx
@@ -0,0 +1,22 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(3, 'Nome deve conter pelo menos 3 letras'),
+  email: z.string().min(1, 'Email é obrigatório').email('Informe um email válido'),
+  password: z.string().min(8, 'Senha deve conter ao menos 8 caracteres')
+})
+type FormData = z.infer<typeof schema>
+
+export function useRegisterController(){
+  const { handleSubmit: hookFormHandleSubmit ,register, formState:{errors} } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  });
+
+  const handleSubmit = hookFormHandleSubmit((data) => {
+    console.log(data)
+  })
+
+  return {handleSubmit, register, errors}
+}
