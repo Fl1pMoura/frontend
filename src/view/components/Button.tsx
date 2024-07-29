dc5b2646feb1cbf4ace468687ec6f80408a351ef
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-07-29 08:23:10 -0300

    feat: create a spinner component to use in login and register component

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,16 +1,23 @@
 import { ComponentProps } from "react";
 import { cn } from "../../app/utils/cn";
+import { Spinner } from "./Spinner";
 
 interface ButtonProps extends ComponentProps<'button'>{
   isLoading?: boolean;
 }
 
-export function Button({isLoading, disabled ,className, ...props }: ButtonProps){
+export function Button({isLoading, disabled ,className, children, ...props }: ButtonProps){
   return (
     <button
       {...props}
       disabled={disabled || isLoading}
-      className={cn("bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed text-white px-6 h-12 rounded-2xl font-medium transition-all", className)}
-    ></button>
+      className={
+        cn(
+          "bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed text-white px-6 h-12 rounded-2xl font-medium transition-all flex justify-center items-center",
+           className)}
+    >
+      {!isLoading && children}
+      {isLoading && <Spinner className="w-6 h-6"/>}
+    </button>
   )
 }
