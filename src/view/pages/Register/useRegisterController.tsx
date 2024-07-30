3af3f9846e6d79a3d94d8363ae104f0283beefc8
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-07-29 23:48:11 -0300

    feat: save access token on local storage / redirect to dashboard if access token is on local storage (login/register)

--- src/view/pages/Register/useRegisterController.tsx
+++ src/view/pages/Register/useRegisterController.tsx
@@ -3,6 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { z } from "zod";
+import { useAuth } from "../../../app/hooks/useAuth";
 import { authService } from "../../../app/services/authService";
 import { SignupParams } from "../../../app/services/authService/signup.";
 
@@ -25,9 +26,12 @@ export function useRegisterController(){
     }
   })
 
+  const { signin } = useAuth();
+
   const handleSubmit = hookFormHandleSubmit(async (data) => {
      try{
-      await mutateAsync(data);
+      const { accessToken } = await mutateAsync(data);
+      signin(accessToken);
      }catch{
       toast.error('ocorreu um erro ao criar sua conta')
      }
