158a19fad55e200b0a5b630851f0bfe7fa4d83ac
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-08-02 00:26:06 -0300

    feat: create UI dashboard account data

--- src/view/pages/Dashboard/components/Accounts.tsx
+++ src/view/pages/Dashboard/components/Accounts.tsx
@@ -1,7 +1,39 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+import { EyeIcon } from "../../../components/icons/EyeIcon";
+
 export function Accounts(){
+  const [isOpen, setIsOpen] = useState(false);
+  function handleShowBalance(){
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="w-full h-full bg-teal-900 rounded-2xl py-8 px-4 lg:p-10 ">
-      Accounts
+    <div className="w-full h-full bg-teal-900 rounded-2xl flex flex-col py-8 px-4 lg:p-10 text-white">
+      <span className="tracking-[-0.5px]">Saldo total</span>
+
+      <div className="flex gap-2 items-center">
+        <strong className="text-2xl tracking-[-1px]">R$ 100,00</strong>
+        <button onClick={handleShowBalance} className="p-2">
+          <EyeIcon open={isOpen}/>
+        </button>
+      </div>
+
+      <div className="flex-1 flex flex-col justify-end">
+          <div className="flex justify-between">
+            <strong className="tracking-[-1px] text-lg">Minhas Contas</strong>
+            <div>
+              <button disabled className="py-3 pr-3.5 pl-2.5 rounded-full transition-colors enabled:hover:bg-black/10 disabled:opacity-40">
+                <ChevronLeftIcon className="h-6 w-6"/>
+              </button>
+              <button className="py-3 pr-3.5 pl-2.5 rounded-full transition-colors enabled:hover:bg-black/10 disabled:opacity-40">
+                <ChevronRightIcon className="h-6 w-6"/>
+              </button>
+            </div>
+          </div>
+          <div>contas</div>
+      </div>
+
     </div>
   )
 }
